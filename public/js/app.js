b93eb64aa071aa38864c0506c72fb6fc95d903ba
Andrew Taghi <aataghi13@gmail.com>
2020-06-20 09:36:03 -0500

    adding routes to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,13 @@ app.controller("MyController", ['$http', function($http) {
   this.skate= 'Stay Grindin'
   this.surf = 'Epic Swells'
   this.snow = 'Shred the Gnar'
+
+  this.createBoard = () => {
+    console.log(this.board);
+  //   $http({
+  //     method:'POST',
+  //     url:'/boards',
+  //     data: {this.createForm}
+  //   })
+  // }
 }]) // END OF APP.CONTROLLER
